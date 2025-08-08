c34c7b2f03ada96120988368809066ccf9c68502
Anjali Mishra <anjali.mishra3537@gmail.com>
2025-08-08 11:33:07 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Expense/',   
   plugins: [react()],
   server: {
-    host: '0.0.0.0', 
-    port: 5173,       
+    host: '0.0.0.0',
+    port: 5173,
   },
 })
